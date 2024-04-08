8115e33d7aa4be01f34d05a8c95f5d616555131f
Hazeljoy08 <hazeljoycollantes@gmail.com>
2024-04-08 20:56:39 +0800

    finalizing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+const Home: React.FC = () => {
+  return (
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Home</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  );
+};
+
+export default Home;
